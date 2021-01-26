6815c07d14761f8f7a8d123839f56bcc679d23a0
codebasics <learnpythonlanguage@gmail.com>
2021-01-26 08:43:12 -0500

    instagram automation

---
+++ 3_instagram/instagram.py
@@ -0,0 +1,19 @@
+from instabot import Bot
+bot = Bot()
+bot.login(username="dhavalsays", password="ienjoy123")
+
+######  upload a picture #######
+bot.upload_photo("yoda.jpg", caption="biscuit eating baby")
+
+######  follow someone #######
+bot.follow("elonrmuskk")
+
+######  send a message #######
+bot.send_message("Hello from Dhaval", ['user1','user2'])
+
+######  get follower info #######
+my_followers = bot.get_user_followers("dhavalsays")
+for follower in my_followers:
+    print(follower)
+
+bot.unfollow_everyone()
